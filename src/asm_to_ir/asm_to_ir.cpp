075739a71de92cff9c4c91cfe844745cc8b77eec
Michael Bagrintsev <michael.bagrintsev@gmail.com>
2026-01-26 15:11:54 +0300

    update

--- src/asm_to_ir/asm_to_ir.cpp
+++ src/asm_to_ir/asm_to_ir.cpp
@@ -16,6 +16,8 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <queue>
+#include <ranges>
 #include <stack>
 #include <stdexcept>
 #include <string>
@@ -137,49 +139,202 @@ std::string_view &expectString(std::string_view &s, const char *expected) {
 }
 }// namespace
 
-struct Function;
+struct Module;
+struct BasicBlock;
+
+struct PHILinker {
+  void addLink(llvm::Value *value, std::vector<BasicBlock *> bbs) {
+    phis.emplace_back(value, std::move(bbs));
+  }
+
+  std::vector<std::pair<llvm::Value *, std::vector<BasicBlock *>>> phis;
+};
 
 constexpr inline size_t kIsaRegFileSize = 16;
-constexpr inline std::string kRegFileName = "regFile";
-
-template<size_t REG_FILE_SIZE = kIsaRegFileSize>
-struct Registers {
-  std::array<RegVal_t, REG_FILE_SIZE> REG_FILE = {};
-};
 
 struct Instruction {
-  virtual void translateToIR(llvm::Module &module, llvm::IRBuilder<> &builder, Function &f) = 0;
+  explicit Instruction(BasicBlock *bb_) : bb(bb_) {}
+
+  virtual void translateToIR(llvm::Module &module, llvm::IRBuilder<> &builder, Module &m) = 0;
   virtual ~Instruction() = default;
+
+ protected:
+  llvm::Value *findLatestRegUsageOrCreateNew(RegId_t reg_id, llvm::IRBuilder<> &builder, Module &m);
+
+ public:
+  // fill when parse
+  BasicBlock *bb;
+
+  // fill when translate to IR
+  llvm::Value *instr_ir = nullptr;
+  std::vector<llvm::Value *> phi_linked_instructions;
 };
+
+// struct
+
+struct BasicBlock {
+ public:
+  using BasicBlockPtr = std::unique_ptr<BasicBlock>;
+  using BasicBlockId = uint32_t;
+
+  static BasicBlockPtr parse(std::string_view &in);
+
+  llvm::Value *findLastRegisterIRValue(RegId_t current_reg_id) {
+    return findLastRegisterIRValue(current_reg_id, instrs.size() - 1);
+  }
+
+  llvm::Value *findLastRegisterIRValue(RegId_t current_reg_id, size_t current_instr_idx) {
+    for (auto [reg_id, val] : instruction_results
+             | std::views::take(current_instr_idx + 1)
+             | std::views::filter([](auto const &o) { return o.has_value(); })
+             | std::views::transform([](auto const &o) { return *o; })
+             | std::views::reverse) {
+      if (reg_id == current_reg_id) {
+        return val;
+      }
+    }
+    return nullptr;
+  }
+
+  Instruction *findLastRegisterValue(RegId_t current_reg_id) {
+    for (auto [reg_id, val] : result_registers
+             | std::views::filter([](auto const &o) { return o.has_value(); })
+             | std::views::transform([](auto const &o) { return *o; })
+             | std::views::reverse) {
+      if (reg_id == current_reg_id) {
+        return val;
+      }
+    }
+    return nullptr;
+  }
+
+  std::vector<BasicBlock *> findRegUsageInBBs(RegId_t current_reg_id) {
+    std::vector<BasicBlock *> result;
+    std::queue<BasicBlock *> nexts;
+    std::unordered_map<BasicBlock *, bool> visited;
+    for (auto &b : pred) {
+      nexts.push(b);
+    }
+    while (nexts.empty()) {
+      auto cur = nexts.front();
+      nexts.pop();
+      visited[cur] = true;
+      if (cur->findLastRegisterValue(current_reg_id)) {
+        result.push_back(cur);
+      } else {
+        for (auto preds_cur : cur->pred) {
+          if (!visited[preds_cur]) {
+            nexts.push(preds_cur);
+          }
+        }
+      }
+    }
+    return result;
+  }
+
+  // fill when parse
+  std::vector<std::unique_ptr<Instruction>> instrs;
+  std::vector<std::string> bb_succ_names;
+  std::vector<std::optional<std::pair<RegId_t, Instruction *>>> result_registers;
+
+  std::vector<BasicBlock *> pred;
+  std::vector<BasicBlock *> succ;
+
+  // fill when translate to ir
+  llvm::BasicBlock *llvm_bb = nullptr;
+  std::vector<std::optional<std::pair<RegId_t, llvm::Value *>>> instruction_results;
+};
+
+struct Function {
+  using FunctionPtr = std::unique_ptr<Function>;
+
+  static FunctionPtr parse(std::string_view &in) {
+    auto f = std::make_unique<Function>();
+    while (true) {
+      in = removeSpaces(in);
+      if (in.at(0) == '.') {
+        expectSymbol(in, '.');
+        auto label = consumeUntilWhitespace(in);
+        f->bbs_ordered.push_back(BasicBlock::parse(in));
+        f->bbs[std::string(label)] = f->bbs_ordered.back().get();
+      } else {
+        break;
+      }
+    }
+    f->setupBBsGraph();
+    return f;
+  }
+
+ private:
+  void setupBBsGraph() {
+    for (auto &bb : bbs_ordered) {
+      for (const auto &succ : bb->bb_succ_names) {
+        bb->succ.push_back(bbs[succ]);
+        bbs[succ]->pred.push_back(bb.get());
+      }
+    }
+  }
+
+ public:
+  std::vector<std::unique_ptr<BasicBlock>> bbs_ordered;
+  std::unordered_map<std::string, BasicBlock *> bbs;
+};
+
+struct Module {
+  using ModulePtr = std::unique_ptr<Module>;
+
+  static ModulePtr parse(std::string_view &in);
+
+  std::unordered_map<std::string, std::unique_ptr<Function>> funcs;
+  PHILinker phi_linker;
+};
+
+llvm::Value *Instruction::findLatestRegUsageOrCreateNew(RegId_t reg_id, llvm::IRBuilder<> &builder, Module &m) {
+  if (auto *val = bb->findLastRegisterValue(reg_id); val != nullptr) {
+    return val;
+  } else {
+    std::vector<BasicBlock *> bbs = bb->findRegUsageInBBs(reg_id);
+    if (bbs.size() == 0) {
+      // TODO: add line in asm code when error occured
+      throw std::runtime_error(std::format("Can't find previous value of register {}", "r" + std::to_string(reg_id)));
+    } else if (bbs.size() == 1) {
+      return bbs[0]->findLastRegisterValue(reg_id);
+    }
+    auto *phi = builder.CreatePHI(builder.getInt32Ty(), bbs.size());
+    m.phi_linker.addLink(phi, std::move(bbs));
+    return phi;
+  }
+}
 
 struct AddInstruction : Instruction {
   using AddInstructionPtr = std::unique_ptr<AddInstruction>;
 
+  size_t instr_idx;
   Bits_t bits;
   RegValue res_reg;
   RegValue arg1_reg;
   Value arg2;
 
-  AddInstruction(Bits_t bits_, RegValue res, RegValue arg1, Value arg2_)
-      : bits(bits_), res_reg(res), arg1_reg(arg1), arg2(arg2_) {}
+  AddInstruction(BasicBlock *bb_, Bits_t bits_, RegValue res, RegValue arg1, Value arg2_)
+      : Instruction(bb_), bits(bits_), res_reg(res), arg1_reg(arg1), arg2(arg2_) {}
 
-  void translateToIR(llvm::Module &module, llvm::IRBuilder<> &builder, Function &) override {
-    auto *regs = module.getGlobalVariable(kRegFileName);
-    auto *arg1_ir = builder.CreateConstGEP2_32(regs->getValueType(), regs, 0, arg1_reg.reg_id);
+  void translateToIR(llvm::Module &, llvm::IRBuilder<> &builder, Module &m) override {
+    llvm::Value *arg1_ir = findLatestRegUsageOrCreateNew(arg1_reg.reg_id, builder, m);
+
     auto *arg2_ir = std::visit(boost::hana::overload(
                                    [&](ImmValue v) {
-                                     // TODO: think about different int types
                                      return static_cast<llvm::Value *>(builder.getInt32(v.value));
                                    },
                                    [&](RegValue v) {
-                                     return builder.CreateConstGEP2_32(regs->getValueType(), regs, 0, v.reg_id);
+                                     return findLatestRegUsageOrCreateNew(v.reg_id, builder, m);
                                    }),
                                arg2);
-    auto *res_ir = builder.CreateConstGEP2_32(regs->getValueType(), regs, 0, res_reg.reg_id);
-    builder.CreateStore(builder.CreateAdd(arg1_ir, arg2_ir), res_ir);
+
+    auto *res_ir = builder.CreateAdd(arg1_ir, arg2_ir);
+    bb->instruction_results[instr_idx].emplace(res_reg.reg_id, res_ir);
   }
 
-  static AddInstructionPtr parse(std::string_view &in) {
+  static AddInstructionPtr parse(std::string_view &in, BasicBlock *bb) {
     in = removeSpaces(in);
     auto bits_sv = consumeUntilWhitespace(in);
     in = removeSpaces(in);
@@ -190,6 +345,7 @@ struct AddInstruction : Instruction {
     auto arg2 = consumeUntilWhitespace(in);
     auto bits = parseNumber<Bits_t>(bits_sv);
     return std::make_unique<AddInstruction>(
+        bb,
         bits,
         RegValue{parseNumber<RegId_t>(res)},
         RegValue{parseNumber<RegId_t>(arg1)},
@@ -199,7 +355,32 @@ struct AddInstruction : Instruction {
   ~AddInstruction() override = default;
 };
 
-struct MulInstruction : Instruction {
+struct JmpInstruction : Instruction {
+  using JmpInstructionPtr = std::unique_ptr<JmpInstruction>;
+
+  BasicBlock *bb;
+  std::string bb_name;
+
+  JmpInstruction(BasicBlock *bb_, std::string bb_name_)
+      : bb(bb_), bb_name(std::move(bb_name_)) {}
+
+  void translateToIR(llvm::Module &, llvm::IRBuilder<> &builder) override {
+    builder.CreateBr(bb->llvm_bb);
+  }
+
+  static JmpInstructionPtr parse(std::string_view &in, BasicBlock *bb) {
+    in = removeSpaces(in);
+    expectSymbol(in, '.');
+    auto label = consumeUntilWhitespace(in);
+    return std::make_unique<JmpInstruction>(
+        bb,
+        std::string(label));
+  }
+
+  ~JmpInstruction() override = default;
+};
+
+/* struct MulInstruction : Instruction {
   using MulInstructionPtr = std::unique_ptr<MulInstruction>;
 
   Bits_t bits;
@@ -325,77 +506,50 @@ struct RetInstruction : Instruction {
   }
 
   ~RetInstruction() override = default;
-};
+}; */
 
-struct BasicBlock : public boost::intrusive::list_base_hook<>, public boost::intrusive::unordered_set_base_hook<> {
- public:
-  using BasicBlockPtr = std::unique_ptr<BasicBlock>;
-  using BasicBlockId = uint32_t;
-
-  static BasicBlockPtr parse(std::string_view in) {
-    auto bb = std::make_unique<BasicBlock>();
-    while (true) {
-      in = removeSpaces(in);
-      auto label = getTokenUntilWhitespace(in);
-      if (in[label.size()] == ':') {
-        // read next function
-        return bb;
-      }
-      if (label.at(0) == '.') {
-        // read next basic block
-        return bb;
-      }
-      if (label == "add") {
-        bb->instrs.push_back(AddInstruction::parse(in));
-      } else if (label == "mul") {
+BasicBlock::BasicBlockPtr BasicBlock::parse(std::string_view &in) {
+  auto bb = std::make_unique<BasicBlock>();
+  while (true) {
+    in = removeSpaces(in);
+    auto label = getTokenUntilWhitespace(in);
+    if (in[label.size()] == ':') {
+      // read next function
+      return bb;
+    }
+    if (label.at(0) == '.') {
+      // read next basic block
+      return bb;
+    }
+    if (label == "add") {
+      bb->instrs.push_back(AddInstruction::parse(in, bb.get()));
+    } else if (label == "jmp") {
+      auto jmp = JmpInstruction::parse(in, bb.get());
+      bb->bb_succ_names.push_back(jmp->bb_name);
+      bb->instrs.push_back(std::move(jmp));
+      break;
+    } /* else if (label == "mul") {
         bb->instrs.push_back(MulInstruction::parse(in));
       } else if (label == "mov") {
         bb->instrs.push_back(MovInstruction::parse(in));
       } else if (label == "ret") {
         bb->instrs.push_back(RetInstruction::parse(in));
-      } else {
-        throw std::runtime_error(std::format("Unknown instruction {}", label.data()));
-      }
+      } */
+    else {
+      throw std::runtime_error(std::format("Unknown instruction {}", label.data()));
     }
   }
+}
 
-  std::vector<std::unique_ptr<Instruction>> instrs;
-};
-
-struct Function {
-  using FunctionPtr = std::unique_ptr<Function>;
-
-  static FunctionPtr parse(std::string_view &in) {
-    auto f = std::make_unique<Function>();
-    while (true) {
-      in = removeSpaces(in);
-      if (in.at(0) == '.') {
-        expectSymbol(in, '.');
-        auto label = consumeUntilWhitespace(in);
-        f->bbs[std::string(label)] = BasicBlock::parse(in);
-      } else {
-        break;
-      }
-    }
-    return f;
+Module::ModulePtr Module::parse(std::string_view &in) {
+  auto module = std::make_unique<Module>();
+  while (!in.empty()) {
+    in = removeSpaces(in);
+    auto function_name = takeUntil(in, ':');
+    module->funcs[std::string(function_name)] = Function::parse(in);
   }
-
-  std::unordered_map<std::string, std::unique_ptr<BasicBlock>> bbs;
-};
-
-class Parser {
- public:
-  void parse(std::string_view input) {
-    while (!input.empty()) {
-      input = removeSpaces(input);
-      auto function_name = takeUntil(input, ':');
-      functions[std::string(function_name)] = Function::parse(input);
-    }
-  }
-
- private:
-  std::unordered_map<std::string, std::unique_ptr<Function>> functions;
-};
+  return module;
+}
 
 }// namespace asm_repr
 
